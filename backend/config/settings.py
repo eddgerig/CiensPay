5e1180bb358bcb027fcc777cec90bdf2c00f2711
eddgerig <edgareduardogerig@gmail.com>
2026-01-19 21:02:09 -0400

    Cambiando settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    
     
     # Local apps
     'rest_framework',
